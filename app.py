c262a07f875c0d39cb9d4f0480c712c8b227d54f
eric-yoshida <84351700+eric-yoshida@users.noreply.github.com>
2022-05-14 12:28:49 -0300

    Desafio: Custom Message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Olá Mundo!"
+    return "Desafio: Custom Message - Completed"
 
 if __name__ == '__main__':
     app.run()
